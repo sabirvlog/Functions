03f32d8790ea0a830e03e3595450f8a4af151445
sabirvlog <sabirkhan0342464@gmail.com>
2022-07-15 16:44:09 +0500

    function1

---
+++ script.js
@@ -0,0 +1,29 @@
+/*
+ function myFun(str){
+   document.getElementById('trial').innerHTML = "Hi Everyone, What's up!";
+ }
+ */
+
+
+// 2nd part................
+
+/*
+function myFun(first,second,third) {	
+	var sum = first + second + third;
+	document.getElementById('trial').innerHTML =
+	"Sum of your values is "+ "<q>"+ sum +"</q>";
+	// document.write(sum);
+	// console.log(sum);
+	alert(sum);
+}
+*/
+
+// 4rth part................
+
+// function myFun(num1, num2) {
+// 	var add = num1 + num2;
+// 	return add;
+// 	// alert(sum);
+// }
+// // the function caller
+// document.getElementById('version').innerHTML = myFun(7, 14, 25);
